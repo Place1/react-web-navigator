4cc7c165e850e2f246e5b8b1f27a09ccbac8a3b1
James Batt <place.net.au@gmail.com>
2016-06-04 11:29:52 +1000

    fixed all style issues

--- src/ReactNavbar/ReactNavbar.js
+++ src/ReactNavbar/ReactNavbar.js
@@ -6,45 +6,47 @@ class ReactNavbar extends React.Component {
 
 	static propTypes = {
 		root: PropTypes.func.isRequired,
-		title: PropTypes.string
+		title: PropTypes.string,
 	}
 
 	static Animations = {
 		push: 'push',
-		pop: 'pop'
+		pop: 'pop',
 	}
 
 	constructor(props) {
 		super(props);
 		this.setInitialState(props);
-	}
-
-	setInitialState(props) {
-		let stack = []
-		if (props.root) {
-			stack.push({
-				component: props.root,
-				title: props.title
-			});
-		}
-		this.state = {
-			stack: stack,
-			animation: props.animation
-		};
+		this.rightClicked = this.rightClicked.bind(this);
+		this.leftClicked = this.leftClicked.bind(this);
 	}
 
 	getCurrent() {
-		if (this.state.stack.length == 0) {
-			return {}
+		if (this.state.stack.length === 0) {
+			return {};
 		}
 		return this.state.stack[this.state.stack.length - 1];
 	}
 
+	setInitialState(props) {
+		const stack = [];
+		if (props.root) {
+			stack.push({
+				component: props.root,
+				title: props.title,
+			});
+		}
+		this.state = {
+			stack,
+			animation: props.animation,
+		};
+	}
+
 	push(component, title = '', animation = ReactNavbar.Animations.push) {
-		this.state.stack.push({component, title});
+		this.state.stack.push({ component, title });
 		this.setState({
 			stack: this.state.stack,
-			animation: animation
+			animation,
 		});
 	}
 
@@ -53,7 +55,7 @@ class ReactNavbar extends React.Component {
 			this.state.stack.pop();
 			this.setState({
 				stack: this.state.stack,
-				animation: animation
+				animation,
 			});
 		}
 	}
@@ -67,15 +69,21 @@ class ReactNavbar extends React.Component {
 	}
 
 	renderLeftItem() {
-		return this.getCurrent().leftItem || (<button onClick={this.leftClicked.bind(this)}>{'<'}</button>);
+		return this.getCurrent().leftItem || (
+			<button onClick={this.leftClicked}>{'<'}</button>
+		);
 	}
 
 	renderMiddleItem() {
-		return this.getCurrent().middleItem || (<span>{this.getCurrent().title}</span>);
+		return this.getCurrent().middleItem || (
+			<span>{this.getCurrent().title}</span>
+		);
 	}
 
 	renderRightItem() {
-		return this.getCurrent().rightItem || (<button onClick={this.rightClicked.bind(this)}>{'#'}</button>);
+		return this.getCurrent().rightItem || (
+			<button onClick={this.rightClicked}>{'#'}</button>
+		);
 	}
 
 	renderComponent() {
